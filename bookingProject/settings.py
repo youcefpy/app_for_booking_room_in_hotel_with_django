f07f07d34dc019efecd70597f77738e142ecfafa
youcefpy <youcefboutiche@hotmail.fr>
2024-07-29 20:39:05 +0200

    integrate postgresSql

--- bookingProject/settings.py
+++ bookingProject/settings.py
@@ -97,9 +97,13 @@ WSGI_APPLICATION = 'bookingProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_booking",
+        "USER": "booking_admin",
+        "PASSWORD": "050299yb",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
